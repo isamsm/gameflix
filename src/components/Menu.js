0372a44a14bc5b2f6d6633a37af3658fe121f8b7
isamsm <isamarsques@gmail.com>
2024-06-06 10:53:25 -0300

    fix: navbar mobile

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,16 +12,16 @@ export const Menu = () => {
             <nav className={`px-12 py-4 fixed top-0 bg-primary w-full z-10 ${isOpen ? "menu w-auto" : ""}`}>
                 <ul className="md:flex lg:flex gap-12">
                     <div className={`${isOpen ? "px-12 py-4" : ""}`}>
-                        <li className="visible md:hidden lg:hidden text-3xl text-secundary absolute" onClick={toggleMenu}> 
+                        <li className="visible md:hidden lg:hidden text-5xl text-secundary absolute" onClick={toggleMenu}> 
                             <AiOutlineMenu />
                         </li>    
                         <li>
-                            <h1 className="pl-12 md:pl-0 lg:pl-0 pb-2 md:pb-0 lg:pb-0 text-3xl font-bebas text-secundary"> Gameflix </h1>
+                            <h1 className="pl-16 md:pl-0 lg:pl-0 pb-2 md:pb-0 lg:pb-0 text-5xl md:text-3xl lg:text-3xl font-bebas text-secundary"> Gameflix </h1>
                         </li>                
                     </div>
                     <li>
                         <ul>
-                            <li className={`flex gap-2 md:gap-5 lg:gap-5 pt-0 md:pt-2 lg:pt-2 text-sm menu-items ${isOpen ? "flex-col open pl-12 pt-5" : ""}`}>
+                            <li className={`flex gap-2 md:gap-5 lg:gap-5 pt-0 md:pt-2 lg:pt-2 text-xl md:text-sm lg:text-sm menu-items ${isOpen ? "flex-col open pl-12 pt-5" : ""}`}>
                                 <a href="#main" className="hover:underline"> Início </a>
                                 <a href="#list" className="hover:underline"> Minha Lista </a>
                                 <a href="#top" className="hover:underline"> Bombando </a>
